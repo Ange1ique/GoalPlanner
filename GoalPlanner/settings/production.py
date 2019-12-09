e171965be2749f8ed4d3bc445c20b80bfc0811cd
Ange1ique <info@angeliqueschouten.com>
2019-12-09 16:07:05 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- GoalPlanner/settings/production.py
+++ GoalPlanner/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['goalplannerapp.herokuapp.com','127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
