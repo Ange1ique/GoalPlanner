4b2f574f69097c76ed30f06cc80dffd1b0145b7e
Ange1ique <info@angeliqueschouten.com>
2019-12-11 13:09:12 +0100

    Changed compress enabled setting to false

--- GoalPlanner/settings/production.py
+++ GoalPlanner/settings/production.py
@@ -47,3 +47,5 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
